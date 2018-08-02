02c7bba1b990aeda5cbac10a875aeefb28490255
lwbtt0915 <lwbtt.0915@163.com>
2018-08-02 17:10:47 +0800

    spring-ampq v1.0

---
+++ mes-ampq/src/main/java/cec/jiutian/service/AmpqProducer.java
@@ -0,0 +1,18 @@
+package cec.jiutian.service;
+
+/**
+ * @Author: lwb
+ * @Description: 消息提供接口
+ * @Date: Created in  17:20 2018/8/1
+ * @modified By:
+ */
+public interface AmpqProducer {
+    /**
+     * 发送消息到指定队列
+     *
+     * @param queueKey
+     * @param object
+     */
+    void sendDataToQueue(String queueKey, Object object);
+
+}
